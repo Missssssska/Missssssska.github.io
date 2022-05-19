5973388c5c1e2c42dde77e1ad44fb1c297fb9e11
Anna <103672021+Missssssska@users.noreply.github.com>
2022-05-19 18:41:47 +0500

    js на проверку
    ошибка

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,8 @@
 "use strict"
 
 
-let calculator = [];
+//let calculator = [];
+let sum = 0;
 
 
 if(confirm("Определились с типом сайта?")){
@@ -9,11 +10,11 @@ if(confirm("Определились с типом сайта?")){
     
 
     let type = {   
-        one: "Визитка - 1000p",
-        two: "Интернет магазин - 3000p",
-        three: "Сайт компании - 4000p",
+        one: "Визитка - 500p",
+        two: "Интернет магазин - 1000p",
+        three: "Сайт компании - 1500p",
         four: "Блог - 2000p",
-        five: "Промо сайт - 3000p",
+        five: "Промо сайт - 2500p",
     };
 
     for (let key in type){
@@ -22,34 +23,34 @@ if(confirm("Определились с типом сайта?")){
     };
 
        
-    let newtype = prompt("Укажите желаемый тип сайта?");
-    console.log(newtype);
+    type = prompt("Укажите желаемый тип сайта?");
+    console.log(type);
     
 
-    let arr =[
-        "уникальный дизайн - 5000p",
-        "шаблонный дизайн - 2000p",
-        "WoW дизайн - 7000p",
+    let design =[
+        "уникальный дизайн - 500p",
+        "шаблонный дизайн - 1000p",
+        "WoW дизайн - 1500p",
     ];
 
-    for (let arrItem of arr){
-        console.log(arrItem);
+    for (let mydesign of design){
+        console.log(mydesign);
     };
 
-    let design = prompt("Укажите желаемый дизайн сайта?");
+    design = prompt("Укажите желаемый дизайн сайта?");
     console.log(design);
 
 
-    let newarr =[
-        "Адаптивная - 7000p",
-        "Не адаптивная - 4000p",
+    let adapt =[
+        "Адаптивная - 4000p",
+        "Не адаптивная - 2000p",
     ];
 
-    for (let arrItems of newarr){
-        console.log(arrItems);
+    for (let myadapt of adapt){
+        console.log(myadapt);
     };
 
-    let adapt = prompt("Укажите желаемую адаптивность?");
+    adapt = prompt("Укажите желаемую адаптивность?");
     console.log(adapt);
    
    
@@ -59,8 +60,41 @@ if(confirm("Определились с типом сайта?")){
     console.log(alert);
 };
 
-/*
-let sum = (type.reduce() + arr.reduce() + newarr.reduce());
-alert("Предварительная стоимость = ");
-console.log(sum); */
 
+
+if (type == 1) {
+    sum = sum + 500;
+} else if (type == 2) {
+    sum = sum + 1000;
+} else if (type == 3) {
+    sum = sum + 1500;
+} else if (type == 4) {
+    sum = sum + 2000;
+} else if (type == 5) {
+    sum = sum + 2500;
+};
+
+
+
+if (design == 1) {
+    sum = sum + 500;
+} else if (design == 2) {
+    sum = sum + 1000;
+} else if (design == 3) {
+    sum = sum + 1500;
+};
+
+
+if (adapt == 1){
+    sum = sum + 4000;
+} else {
+    (adapt == 2);
+    sum = sum + 2000;
+};
+
+
+function calculator (type, design, adapt) {
+    return type + design + adapt;
+}
+let result = calculator (type, design, adapt);
+alert (calculator);
