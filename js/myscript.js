6a6bbaaa913a4e54921fed3ee89ecd8102df854b
Anna <103672021+Missssssska@users.noreply.github.com>
2022-05-28 17:18:18 +0500

    дз по 11 и 12 урокам

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,7 @@
 
 //let calculator = [];
 let sum = 0;
+let date = 5;
 
 
 if(confirm("Определились с типом сайта?")){
@@ -23,9 +24,25 @@ if(confirm("Определились с типом сайта?")){
     };
 
        
-    type = prompt("Укажите желаемый тип сайта?");
+    type = prompt("Укажите желаемый тип сайта?, 1 - Сайт-Визитка, 2 - Интернет магазин, 3 - Корпоративный сайт, 4 - Блог, 5 - Промо сайт");
     console.log(type);
     
+        if (type == 1) {
+        sum = sum + 500;
+        date = date + 2;
+    } else if (type == 2) {
+        sum = sum + 1000;
+        date = date + 3;
+    } else if (type == 3) {
+        sum = sum + 1500;
+        date = date + 4;
+    } else if (type == 4) {
+        sum = sum + 2000;
+        date = date + 5;
+    } else if (type == 5) {
+        sum = sum + 2500;
+        date = date + 4;
+    };
 
     let design =[
         "уникальный дизайн - 500p",
@@ -37,8 +54,20 @@ if(confirm("Определились с типом сайта?")){
         console.log(mydesign);
     };
 
-    design = prompt("Укажите желаемый дизайн сайта?");
+    design = prompt("Укажите желаемый дизайн сайта?, 1 - уникальный дизайн, 2 - шаблонный дизайн, 3 - WoW дизайн");
     console.log(design);
+    
+        if (design == 1) {
+        sum = sum + 500;
+        date = date + 3;
+    } else if (design == 2) {
+        sum = sum + 1000;
+        date = date + 3;
+    } else if (design == 3) {
+        sum = sum + 1500;
+        date = date + 3;
+    };
+
 
 
     let adapt =[
@@ -50,9 +79,20 @@ if(confirm("Определились с типом сайта?")){
         console.log(myadapt);
     };
 
-    adapt = prompt("Укажите желаемую адаптивность?");
+    adapt = prompt("Укажите желаемую адаптивность?, 1 - Адаптивная, 2 - Не адаптивная");
     console.log(adapt);
-   
+    
+    
+    if (adapt == 1){
+        sum = sum + 4000;
+        date = date + 7;
+    } else {
+        (adapt == 2);
+        sum = sum + 2000;
+        date = date + 5;
+    };
+
+  alert("Предварительная стоимость сайта: " + sum + " руб. " + date + " дней."); 
    
     
 } else{
@@ -62,39 +102,91 @@ if(confirm("Определились с типом сайта?")){
 
 
 
-if (type == 1) {
-    sum = sum + 500;
-} else if (type == 2) {
-    sum = sum + 1000;
-} else if (type == 3) {
-    sum = sum + 1500;
-} else if (type == 4) {
-    sum = sum + 2000;
-} else if (type == 5) {
-    sum = sum + 2500;
+$(document).ready(function(){
+    
+                  
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    $(".section").each((i, el) => {
+        
+        if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+            $("nav a").each((i, el) => {
+                if ($(el).hasClass("active")){
+                    $(el).removeClass("active");
+                }
+            });
+            
+            $('nav li:eq('+ i +')').find('a').addClass('active');
+        }
+    });     
+});
+    
+});
+
+
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top - 50 + "px"})
+    
+});
+
+
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+
+//---------ОШИБКА------------
+$(document).ready(function(){
+    
+    
+    let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver( onEntry, options);
+    let element = $('.element-animation');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+});
+
+    function onEntry(entry){
+    entry.forEach(change => {
+        if(change.isIntersecting){
+            change.target.classList.add('show-animation');
+        }
+    });
+    };
+
+
+
+$('select').change(summ);
+
+function summ(){
+    let result=0;
+    
+    $('#sum').find('select').each(function(){
+        let value = 0;
+
+        if (typeof $(this).val() == 'object'){
+          $.each($(this).val(), function(index, val) {
+            value += val*1;
+          });
+        } else {
+          value = $(this).val()
+        }
+        result +=value*1;
+
+    });
+    $('#table_sum').html(result + ' руб.');
 };
 
 
-
-if (design == 1) {
-    sum = sum + 500;
-} else if (design == 2) {
-    sum = sum + 1000;
-} else if (design == 3) {
-    sum = sum + 1500;
-};
-
-
-if (adapt == 1){
-    sum = sum + 4000;
-} else {
-    (adapt == 2);
-    sum = sum + 2000;
-};
+$('select').change(function(){
+    let sum_date = 0;
+    $('select').each(function() {
+        sum_date += $(this).find(':selected').data('value');
+    });
+     $("#table_date").html(sum_date + ' дней.');
+});
 
 
-function calculator (type, design, adapt) {
-    return type + design + adapt;
-}
-let result = calculator (type, design, adapt);
-alert (calculator);
+
